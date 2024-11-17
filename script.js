59c72b7651a399066b69f83c7ae4056f0f370274
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2024-11-17 17:49:48 -0300

    we are so barack

--- script.js
+++ script.js
@@ -2,43 +2,111 @@
 // @name         Alura Destroyer
 // @namespace    https://cursos.alura.com.br/
 // @version      2024-09-23
-// @description  nuh uh i dont want to do alura nomore :sob:
-// @author       wwwwwwwwwww
+// @description  eu to cansado jefe é mt coisakkkk
+// @author       marcos10pc
 // @match        https://cursos.alura.com.br/course/*/task/*
 // @icon         https://i.imgur.com/gP1LZq9.png
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    const water_mark = document.querySelector('.formattedText');
-    water_mark.innerHTML = 'sussy baka amongus';
+// Tem que ta passando muita fome mesmo pra modificar script gratuito e vender pros outros
 
-    let cookies = document.cookie;
-
-    let actual_url = window.location.href;
-
-    let next_lesson_button = document.getElementsByClassName("bootcamp-next-button")[0];
-
-    if (next_lesson_button){
-        let next_lesson_link = next_lesson_button.getAttribute('href');
-        let parts = actual_url.split('/');
-        let lessonName = parts[4];
-        let lessonId = parts[6];
-        console.log(`[DEBUG] Lesson_Name: ${lessonName} Lesson_Id: ${lessonId} `);
-
-        fetch(`https://cursos.alura.com.br/course/${lessonName}/task/${lessonId}/mark-video`, {
-          method: 'POST',
-          credentials: 'include',
-          headers: {
-             'Content-Type': 'application/json',
-             'Cookie': cookies
-          }}).then(data => {
-          console.log("[DEBUG] Lesson Done!")
-          })
-
-        setTimeout(next_lesson_button.click(), 4000); // Modificar o tempo para 4 segundos
-    } else {
-        alert("Next Lesson Button not found :( are u sure that u are on the correct page?");
+(function (_0x2c471e, _0x237ff3) {
+    const _0x4d0918 = _0x34d6;
+    const _0x1f7940 = _0x2c471e();
+    while (!![]) {
+        try {
+            const _0x57d037 = parseInt(_0x4d0918(0x16c)) / 0x1 * (parseInt(_0x4d0918(0x169)) / 0x2) + parseInt(_0x4d0918(0x158)) / 0x3 * (-parseInt(_0x4d0918(0x153)) / 0x4) + parseInt(_0x4d0918(0x163)) / 0x5 * (parseInt(_0x4d0918(0x152)) / 0x6) + -parseInt(_0x4d0918(0x154)) / 0x7 * (parseInt(_0x4d0918(0x16d)) / 0x8) + parseInt(_0x4d0918(0x162)) / 0x9 * (parseInt(_0x4d0918(0x159)) / 0xa) + -parseInt(_0x4d0918(0x161)) / 0xb * (-parseInt(_0x4d0918(0x167)) / 0xc) + parseInt(_0x4d0918(0x168)) / 0xd;
+            if (_0x57d037 === _0x237ff3) {
+                break;
+            } else {
+                _0x1f7940['push'](_0x1f7940['shift']());
+            }
+        } catch (_0x8e145) {
+            _0x1f7940['push'](_0x1f7940['shift']());
+        }
     }
-})();
+}(_0x28f8, 0x5c95f));
+function _0x28f8() {
+    const _0x3427c4 = [
+        'getElementsByClassName',
+        '12rRkCdq',
+        '365807edsUGZ',
+        '19458vovUKN',
+        'POST',
+        '.alternativeList',
+        '25ZnJDug',
+        '8FvGyoG',
+        'querySelectorAll',
+        'task-actions-button',
+        '18EdKqjW',
+        '1683188IomIcr',
+        '2752274BZNFJP',
+        'click',
+        '.formattedText',
+        'dmFvIHNlIGZ1ZGVyZW0gfCBtYWRlIGJ5IG1hcmNvczEwcGMgfCBkaXNjb3JkLmdnL3BsYXRmb3JtZGVzdHJveWVyIHwgc2UgdmMgcGFnb3UgcG9yIGlzc28gdmMgZm9pIHNjYW1tYWRv',
+        '3AfDXMp',
+        '10JTPCgI',
+        'include',
+        'href',
+        'getAttribute',
+        'li[data-correct=\x22true\x22]',
+        'then',
+        'https://cursos.alura.com.br/course/',
+        'forEach',
+        '1266023NQrbAg',
+        '2898981mrsjrY',
+        '807740FPEAsL',
+        'querySelector',
+        'location'
+    ];
+    _0x28f8 = function () {
+        return _0x3427c4;
+    };
+    return _0x28f8();
+}
+function _0x34d6(_0x1ee3f8, _0x54aa1d) {
+    const _0x28f8ab = _0x28f8();
+    _0x34d6 = function (_0x34d632, _0x52328c) {
+        _0x34d632 = _0x34d632 - 0x151;
+        let _0x33e026 = _0x28f8ab[_0x34d632];
+        return _0x33e026;
+    };
+    return _0x34d6(_0x1ee3f8, _0x54aa1d);
+}
+(function () {
+    'use strict';
+    const _0x233586 = _0x34d6;
+    const _0x2f06b4 = document[_0x233586(0x164)](_0x233586(0x156));
+    _0x2f06b4['innerHTML'] = atob(_0x233586(0x157));
+    let _0x1c4064 = window[_0x233586(0x165)][_0x233586(0x15b)];
+    let _0x59d3c3 = document[_0x233586(0x166)]('bootcamp-next-button')[0x0];
+    if (_0x59d3c3) {
+        let _0x211375 = _0x59d3c3[_0x233586(0x15c)]('href');
+        let _0x411637 = _0x1c4064['split']('/');
+        let _0x5e826b = _0x411637[0x4];
+        let _0x3b7e55 = _0x411637[0x6];
+        fetch(_0x233586(0x15f) + _0x5e826b + '/task/' + _0x3b7e55 + '/mark-video', {
+            'method': _0x233586(0x16a),
+            'credentials': _0x233586(0x15a),
+            'headers': { 'Content-Type': 'application/json' }
+        })[_0x233586(0x15e)](_0x14c092 => {
+        });
+        const _0x42dac4 = document[_0x233586(0x164)](_0x233586(0x16b));
+        if (_0x42dac4) {
+            const _0x578c7a = _0x42dac4[_0x233586(0x16e)](_0x233586(0x15d));
+            _0x578c7a[_0x233586(0x160)](_0x153e0d => {
+                const _0x578782 = _0x233586;
+                const _0x279285 = _0x153e0d[_0x578782(0x164)]('input[type=\x22radio\x22]');
+                if (_0x279285) {
+                    _0x279285[_0x578782(0x155)]();
+                }
+            });
+        }
+        const _0x35d957 = document[_0x233586(0x166)](_0x233586(0x151))[0x0];
+        if (_0x35d957) {
+            _0x35d957[_0x233586(0x155)]();
+        }
+        setTimeout(_0x59d3c3['click'](), 0x1388);
+    }
+}());
