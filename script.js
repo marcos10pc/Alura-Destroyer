84b43e36f194a35cb823f213a31dcc9bd4b8e933
marcos10pc <30839781+marcos10pc@users.noreply.github.com>
2024-12-05 12:22:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Alura Destroyer
 // @namespace    https://cursos.alura.com.br/
-// @version      2024-09-23
-// @description  eu to cansado jefe Ã© mt coisakkkk
+// @version      2024-05-12
+// @description  eu to cansado jefe é mt coisakkkk
 // @author       marcos10pc
 // @match        https://cursos.alura.com.br/course/*/task/*
 // @icon         https://i.imgur.com/gP1LZq9.png
 // @grant        none
+// @require      https://raw.githubusercontent.com/marcos10pc/Alura-Destroyer/refs/heads/main/hawk_tuah.js
 // ==/UserScript==
 
-// Tem que ta passando muita fome mesmo pra modificar script gratuito e vender 
-
-(function(_0x1abd98,_0x20ec75){const _0x44ea2e=_0x5988;const _0x1704d7=_0x1abd98();while(!![]){try{const _0x50c221=parseInt(_0x44ea2e(0x1d3))/0x1*(-parseInt(_0x44ea2e(0x1ce))/0x2)+parseInt(_0x44ea2e(0x1e4))/0x3*(parseInt(_0x44ea2e(0x1c7))/0x4)+parseInt(_0x44ea2e(0x1c8))/0x5*(-parseInt(_0x44ea2e(0x1e6))/0x6)+parseInt(_0x44ea2e(0x1d2))/0x7*(-parseInt(_0x44ea2e(0x1d0))/0x8)+parseInt(_0x44ea2e(0x1e5))/0x9+-parseInt(_0x44ea2e(0x1dc))/0xa+-parseInt(_0x44ea2e(0x1cf))/0xb*(-parseInt(_0x44ea2e(0x1dd))/0xc);if(_0x50c221===_0x20ec75){break}else{_0x1704d7['push'](_0x1704d7['shift']())}}catch(_0x2f205c){_0x1704d7['push'](_0x1704d7['shift']())}}})(_0x2b4d,0xb4882);function _0x5988(_0x36c7da,_0x4092bc){const _0x2b4df=_0x2b4d();_0x5988=function(_0x598896,_0x5013de){_0x598896=_0x598896-0x1c6;let _0x6f0386=_0x2b4df[_0x598896];return _0x6f0386};return _0x5988(_0x36c7da,_0x4092bc)}function _0x2b4d(){const _0x14ba78=['554298cguQrE','a.task-actions-button-next','getElementsByClassName','8ktQjRd','55HVBldJ','getAttribute','/mark-video','.block','innerHTML','href','2LFhdVr','286xMMddo','88vTyiDv','application/json','830879zGrNWz','257171rveNGv','location','then','.formattedText','click','sortBlocksOrigin','.alternativeList','querySelectorAll','querySelector','5706130OibDZe','1658148xpuVUf','/task/','input[type="radio"]','forEach','bootcamp-next-button','split','dmFvIHNlIGZ1ZGVyZW0gfCBtYWRlIGJ5IG1hcmNvczEwcGMgfCBkaXNjb3JkLmdnL3BsYXRmb3JtZGVzdHJveWVyIHwgc2UgdmMgcGFnb3UgcG9yIGlzc28gdmMgZm9pIHNjYW1tYWRv','237444tOXIPX','1243548QDuCxH'];_0x2b4d=function(){return _0x14ba78};return _0x2b4d()}(function(){'use strict';const _0xb40b8e=_0x5988;const _0x35dff3=document[_0xb40b8e(0x1db)](_0xb40b8e(0x1d6));_0x35dff3[_0xb40b8e(0x1cc)]=atob(_0xb40b8e(0x1e3));let _0x5ed3fe=window[_0xb40b8e(0x1d4)][_0xb40b8e(0x1cd)];let _0x338873=document[_0xb40b8e(0x1c6)](_0xb40b8e(0x1e1))[0x0];if(_0x338873){let _0x2dbd25=_0x338873[_0xb40b8e(0x1c9)](_0xb40b8e(0x1cd));let _0x193cef=_0x5ed3fe[_0xb40b8e(0x1e2)]('/');let _0x16eead=_0x193cef[0x4];let _0x195867=_0x193cef[0x6];fetch('https://cursos.alura.com.br/course/'+_0x16eead+_0xb40b8e(0x1de)+_0x195867+_0xb40b8e(0x1ca),{'method':'POST','credentials':'include','headers':{'Content-Type':_0xb40b8e(0x1d1)}})[_0xb40b8e(0x1d5)](_0x58b199=>{});const _0x3f809b=document[_0xb40b8e(0x1db)](_0xb40b8e(0x1d9));if(_0x3f809b){const _0x1746af=_0x3f809b[_0xb40b8e(0x1da)]('li[data-correct="true"]');_0x1746af[_0xb40b8e(0x1e0)](_0xba8e1e=>{const _0x1c2ee0=_0xb40b8e;const _0xcce373=_0xba8e1e[_0x1c2ee0(0x1db)](_0x1c2ee0(0x1df));if(_0xcce373){_0xcce373[_0x1c2ee0(0x1d7)]()}})}const _0x532c1b=document[_0xb40b8e(0x1c6)]('task-actions-button')[0x0];if(_0x532c1b){_0x532c1b['click']()}const _0x18b315=document['getElementById'](_0xb40b8e(0x1d8));if(_0x18b315){const _0x40ca78=_0x18b315[_0xb40b8e(0x1da)](_0xb40b8e(0x1cb));_0x40ca78[_0xb40b8e(0x1e0)](_0x3298f8=>_0x3298f8[_0xb40b8e(0x1d7)]())}const _0x69e216=document['getElementById']('submitBlocks');if(_0x69e216){_0x69e216['click']()}const _0x349e04=document[_0xb40b8e(0x1db)](_0xb40b8e(0x1e7));if(_0x349e04){_0x349e04[_0xb40b8e(0x1d7)]()}setTimeout(_0x338873['click'](),0x1388)}}());
+(function() {
+    'use strict';
+    console.log("https://pixgg.com/marcos10pc"); 
+})();
